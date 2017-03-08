754863254a70569a5056b1cabdacf01fdab44b12
Carlos Anguiano <carlos.anguiano@gmail.com>
2017-03-07 23:43:52 -0600

    FileReaderOne
    FileReaderOne

---
+++ src/filereaderone/FileReaderOne.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filereaderone;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author carlosjoseanguiano
+ */
+public class FileReaderOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try{
+            //Abro stream, crea el fichero si no existe
+            FileWriter fw=new FileWriter("D:\\fichero1.txt");
+            //Escribimos en el fichero un String y un caracter 97 (a)
+            fw.write("Esto es una prueba");
+            fw.write(97);
+            //Cierro el stream
+            fw.close(); 
+                //Abro el stream, el fichero debe existir
+            FileReader fr=new FileReader("D:\\fichero1.txt");
+            //Leemos el fichero y lo mostramos por pantalla
+            int valor=fr.read();
+            while(valor!=-1){
+                System.out.print((char)valor);
+                valor=fr.read();
+            }
+            //Cerramos el stream
+            fr.close();
+        }catch(IOException e){
+            System.out.println("Error E/S: "+e);
+        }
+    }
+}
+    
+
